25a58cce9d112494ad2e97af0365c6b2bfe3d9b9
Harshit <harsjoshi1614@gmail.com>
2024-05-10 08:15:26 +0000

    codesandbox config files

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,50 +1,51 @@
-import {submitBehaviour, useSingleInput} from "../../hooks/input";
-import {type FC} from "react";
-import {TaskI} from "../../types/task";
+import { submitBehaviour, useSingleInput } from "../../hooks/input";
+import { type FC } from "react";
+import { TaskI } from "../../types/task";
 
 import css from "./TodoForm.module.css";
-import {extract_tags} from "../../utils/tasks";
+import { extract_tags } from "../../utils/tasks";
 
 const PLACEHOLDER_TEXT = "Enter your task ! Press Enter to submit.";
 
 interface props {
-	addTask: (t: TaskI) => void;
+  addTask: (t: TaskI) => void;
 }
 
-export const TodoForm: FC<props> = ({addTask}) => {
-	// todo: use a useRef to abstract away handle* functions
+export const TodoForm: FC<props> = ({ addTask }) => {
+  // todo: use a useRef to abstract away handle* functions
 
-	const onSubmit = (text: string) => {
-		const tags = extract_tags(text);
-		addTask({
-			// * Apparently a weird bug happens when i use `id` as the key lmao
-			// why does this happend :hmm
-			id: Math.random() * 1000,
-			isDone: false,
-			tags,
-			text
-		});
-	};
+  const onSubmit = (text: string) => {
+    // JUMP: this is where i add a Task
+    const tags = extract_tags(text);
+    addTask({
+      // * Apparently a weird bug happens when i use `id` as the key lmao
+      // why does this happend :hmm, I AM NOT USING THIS AS KEY FOR NOW
+      id: Math.random() * 1000,
+      isDone: false,
+      tags,
+      text,
+    });
+  };
 
-	const [title, , handleChange, submitOnEnter] = useSingleInput(
-		"",
-		onSubmit,
-		submitBehaviour.DEFAULT
-	);
+  const [title, , handleChange, submitOnEnter] = useSingleInput(
+    "",
+    onSubmit,
+    submitBehaviour.DEFAULT,
+  );
 
-	return (
-		<div className={css.form}>
-			<input
-				type="text"
-				autoFocus
-				placeholder={PLACEHOLDER_TEXT}
-				value={title}
-				onChange={handleChange}
-				onKeyDown={submitOnEnter}
-			/>
-			{/* <Cursor offset={10}></Cursor> */}
-		</div>
-	);
+  return (
+    <div className={css.form}>
+      <input
+        type="text"
+        autoFocus
+        placeholder={PLACEHOLDER_TEXT}
+        value={title}
+        onChange={handleChange}
+        onKeyDown={submitOnEnter}
+      />
+      {/* <Cursor offset={10}></Cursor> */}
+    </div>
+  );
 };
 
 // const Cursor: FC<{offset: number}> = ({offset}) => {
