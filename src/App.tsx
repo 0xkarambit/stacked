7ca050e5d31656242652df93754d1e2b939d4fc7
Harshit <harsjoshi1614@gmail.com>
2024-05-14 21:10:29 +0530

    Added ctrl + h to hide the header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import styles from "./styles.module.css";
 import {Header} from "./components/Header";
 import {TodoForm} from "./components/TodoForm";
 import {TaskStacks} from "./components/TodoStacks";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-	const [headerShow] = useState<boolean>(true);
+	const [headerShow, setHeaderState] = useState<boolean>(true);
+	useEffect(() => {
+		const listener = (e: KeyboardEvent) => {
+			console.log(e);
+			if (e.key == "h" && e.ctrlKey) {
+				e.preventDefault();
+				setHeaderState(s => !s);
+			}
+		};
+		document.addEventListener("keydown", listener);
+		return () => {
+			removeEventListener("keydown", listener);
+		};
+	}, []);
 	return (
 		<>
 			{headerShow && <Header></Header>}
